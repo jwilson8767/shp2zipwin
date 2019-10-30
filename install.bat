5c8f50779976538cfd0cf73e7c76378dcc77d8ca
Joshua Wilson <jwilson@fernleafinteractive.com>
2019-10-30 12:47:47 -0400

    close ps window when finished

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 
-FileActionsConsole.exe add shp shpzip "Zip Shapefile" "Powershell.exe -noexit -File shp2zip.ps1 "%%1"" [default=false]
+FileActionsConsole.exe add shp shpzip "Zip Shapefile" "Powershell.exe -File shp2zip.ps1 "%%1"" [default=false]
 copy shp2zip.ps1 "%appdata%\\ShellExtensions\shp2zip.ps1" /Y
 
 pause
